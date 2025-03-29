f404522e84d53215caef8cecd97c7cc4b9879b62
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-03-29 09:50:13 -0300

    7. Interação entre Elementos do Tkinter

--- Aula06.py
+++ Aula06.py
@@ -16,13 +16,26 @@ mensagem2.grid(row=1, column=0)
 moeda = tk.Entry()
 moeda.grid(row=1, column=1)
 
+
+#dicionario simula api moeda
+dicionario_cotacoes = {
+  'Dolar': 5.70,
+  'Euro': 6.70,
+  'Libra': 8.70,
+  
+}
 #funcao que pega o texto do campo moeda ao clicar no botao
 def buscar_cotacao():
-  print(moeda.get())
+  moeda_pesquisada = moeda.get()
+  cotacao_moeda = dicionario_cotacoes.get(moeda_pesquisada)
+  mensagem_resposta = tk.Label(text="Cotação não encontrada!")
+  mensagem_resposta.grid(row=3, column=1)
+  if cotacao_moeda:
+    mensagem_resposta['text'] = f'Cotação do {moeda_pesquisada} é de {cotacao_moeda} $Reais'
 
 
 botao = tk.Button(text="Buscar cotação!", command=buscar_cotacao)
-botao.grid(row=3, column=1)
+botao.grid(row=3, column=0)
 
 janela.mainloop()
 
